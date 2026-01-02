3b52783e2b5d7c9ed03d224d1bf2ec3ad24f3d19
mack <jihanyang123@163.com>
2026-01-03 00:19:13 +0800

    refactor(openfile): 重构文件对话框类，优化数据结构和方法实现
    - 使用统一的数据结构存储文件信息
    - 拆分目录和文件列表获取逻辑
    - 重命名数据发送方法为更简洁的send
    - 添加类型注解提升代码可读性

--- src/servers/api/openfile.py
+++ src/servers/api/openfile.py
@@ -7,44 +7,79 @@ class OpenFileDialog:
     文件导入窗口后台执行支持
     """
     def __init__(self):
+        self.name:str|None = None
+        self.path:str|None = None
+        self.is_dir:bool|None = None
+        self.size:int|None = 0
+        self.modified:str|float|None = None
+        self.ext:str|None = None
+
+        self.data = {
+            'name': self.name,
+            'path': self.path,
+            'is_dir': self.is_dir,
+            'size': self.size,
+            'modfied': self.modified,
+            'ext': self.ext
+        }
+
         self.default = ""
-        self.drive = []
-        self.dir_list = []
         self.msgsend = MessagesRepeat()
 
     def get_drive_letter(self):
         # 获取驱动器列表
         check_mask = [3]
+        drive_list = []
         for i in range(26):
             drive = fr'{chr(ord('A')+i)}:\\'
             mask = win32file.GetDriveType(drive)
             if mask in check_mask:
-                path = Path(drive).resolve()
-                self.drive.append(path)
-        self.data_send('dirve_list',self.drive)
+                self.name = mask
+                self.path = str(Path(drive).resolve())
+                self.is_dir = True
+                drive_list.append(self.data)
+        self.send('file.open.dirve_list', drive_list)
 
     def get_dir_list(self,path):
-        # 获取目录列表
-        if path and Path(path).is_dir():
-            for item in Path(path).iterdir():
-                self.dir_list.append(item)
-            self.data_send('file.dirlist',self.dir_list)
-            self.dir_list = []
-        elif path and Path(path).is_file():
-            return
-        elif not path:
-            return
+        """
+        获取目录列表
+        :param path:
+        :return:
+        """
+        file = Path(path)
+        dir_list = []
+        for item in file.iterdir():
+            if item.is_dir():
+                self.data['name'] = item.name
+                self.data['path'] = str(Path(item).resolve())
+                self.data['is_dir'] = True
+                dir_list.append(self.data)
+        self.send('file.open.dir_list',dir_list)
 
-    def get_file_path(self):
-        pass
 
-    def get_file_list(self):
-        pass
+    def get_file_list(self,path):
+        """
+        获取目标目录文件列表
+        :return:
+        """
+        file_list = []
+        file = Path(path)
+        if file.is_dir():
+            for item in file.iterdir():
+                if item.is_file():
+                    self.name = item.name
+                    self.path = str(item.resolve())
+                    self.is_dir = False
+                    self.size = item.stat().st_size
+                    self.modified = item.stat().st_mtime
+                    self.ext = item.suffix
+                    file_list.append(self.data)
+            self.send('file.open.file_list',file_list)
 
     def read_file_filter(self):
         pass
 
-    def data_send(self,type,message):
+    def send(self,type,message):
         """
         发送文件数据信息
         :return:
